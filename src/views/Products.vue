477cbc46b41b8746a090bf7f29117cd8906e363e
wendt <wequispe@americatv.pe>
2019-04-27 13:03:00 -0500

    agregando componente cards y la vista de productos

---
+++ src/views/Products.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <b-row>
+      <b-col v-for="(user, index) in allUsers " :key="index">
+
+        <!-- {{ user }} -->
+        <Cards :user="user" ></Cards>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import Cards from '@/components/Cards.vue'
+
+export default {
+  name: 'products',
+  components: {
+    Cards
+  },
+  data() {
+   return {
+    //  allUsers: this.$store.state.allUsers
+   }
+  },
+  methods: {
+
+  },
+  computed: {
+    allUsers() {
+      let users = this.$store.state.allUsers
+      return users
+    }
+  }
+}
+</script>
+
