c70a4bbc9fc06868712fd6b7d15948ee664b8140
Milu <mrobles@americatv.pe>
2019-04-27 11:32:47 -0500

    Agregando bootstrap

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router/routes'
 import axios from 'axios'
 import store from './store/index'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use( axios)
 
@@ -10,6 +13,7 @@ export const HTTP = axios.create({
   baseURL: `https://randomuser.me/api/`
 })
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
